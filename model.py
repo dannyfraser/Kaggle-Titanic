bf53444f9c2fd5c77d3979d02a4ba49f926315e9
Danny Fraser <danny.fraser@woodmac.com>
2015-08-14 11:50:26 +0100

    applied fare ceiling

--- model.py
+++ model.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 from pandas import Series, DataFrame
 
-train = pd.read_csv('data/train.csv')	
+train = pd.read_csv('data/train.csv')
+
+#set up ticket price bins
+fare_ceiling = 40
+idx = pd.IndexSlice
+mask = train.loc[:, idx['Fare']] > fare_ceiling
+train[mask] = fare_ceiling - 1
